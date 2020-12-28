2dc49720f84930fbf8502b51ebb0dad379aaf88d
Wenderson Fernandes <oldmaverick@gmail.com>
2020-12-28 18:39:47 -0300

    Completed lesson 07 Customizing Your Design System from Adam Wathan tailwind tutorial

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+		extend: {
+			colors: {
+				'brand-blue' : '#1992d4'
+			},
+			spacing: {
+				'72' : '18rem',
+			}
+		},
     screens: {
       sm: '640px',
       md: '768px',
